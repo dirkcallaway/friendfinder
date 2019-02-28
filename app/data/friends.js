a96bdc4e74923d124f0d12f06b9288242e152407
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-02-27 18:53:27 -0700

    Updated survey and added new characters

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,13 +2,13 @@
 //=============================================================
 var friendArr = [
   {
-  name: "Ahmed",
-  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  name: "Hermione",
+  photo: "https://vignette.wikia.nocookie.net/harrypotter/images/4/41/Hermionedhface.jpg/revision/latest/scale-to-width-down/350?cb=20161221044857",
   scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 },
 {
-  name: "Josie",
-  photo: "https://get.pxhere.com/photo/person-girl-woman-hair-photography-female-portrait-model-young-studio-fashion-black-lady-hairstyle-long-hair-make-up-black-dress-black-hair-face-dress-eye-head-skin-lips-pose-beauty-beautiful-sexy-style-elegant-pretty-posture-photoshoot-seductive-attractive-glamour-stylish-photo-shoot-brown-hair-portrait-photography-art-model-supermodel-916514.jpg",
+  name: "Ron",
+  photo: "https://upload.wikimedia.org/wikipedia/en/thumb/5/5e/Ron_Weasley_poster.jpg/220px-Ron_Weasley_poster.jpg",
   scores: [2, 4, 5, 1, 3, 2, 3, 1, 1, 4]
 },
 {
@@ -17,8 +17,8 @@ var friendArr = [
   scores: [2, 1, 2, 2, 3, 4, 1, 5, 3, 2]
 },
 {
-  name: "Jane",
-  photo: "https://get.pxhere.com/photo/person-people-girl-woman-hair-photography-cute-female-singer-brunette-portrait-model-young-human-fashion-lady-lip-hairstyle-makeup-feminine-eyebrow-mouth-long-hair-make-up-close-up-human-body-posing-black-hair-face-nose-eyes-lovely-eye-head-look-skin-lips-pose-beauty-beautiful-sexy-style-pretty-organ-femininity-attractive-elegance-lipstick-adult-glamour-stylish-vogue-photo-shoot-cosmetic-brown-hair-portrait-photography-art-model-supermodel-913380.jpg",
+  name: "Luna",
+  photo: "https://images.pottermore.com/bxd3o8b291gf/t6GVMDanqSKGOKaCWi8oi/2a4c50ed04600e4e4c0c5012628f4ace/LunaLovegood_WB_F5_LunaLovegoodPromoCloseUp_Promo_080615_Port.jpg?w=727&h=409&fit=thumb&f=face&q=85",
   scores: [4, 3, 3, 1, 2, 2, 5, 3, 3, 1]
 },
 ];
